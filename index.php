f22a5e119cca364e78d05911cd8f9e6eb1e3b7d4
Paul Fillafer <pfillafer@tsn.at>
2024-11-06 21:58:05 +0100

    Index mit Button und Feldern

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Eingabeformular</title>
+</head>
+<body>
+<div style="max-width: 300px; margin: auto; padding-top: 50px;">
+    <h2>Eingabeformular</h2>
+    <form action="zielseite.php" method="post">
+        <div>
+            <label for="feld1">Eingabefeld 1:</label>
+            <input type="text" id="feld1" name="feld1" required>
+        </div>
+        <div style="margin-top: 10px;">
+            <label for="feld2">Eingabefeld 2:</label>
+            <input type="text" id="feld2" name="feld2" required>
+        </div>
+        <div style="margin-top: 20px;">
+            <button type="submit">Abschicken</button>
+        </div>
+    </form>
+</div>
+</body>
+</html>
