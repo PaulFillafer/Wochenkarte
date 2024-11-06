847d462f99bf658e5e965279a0d18bdb92109103
Paul Fillafer <pfillafer@tsn.at>
2024-11-06 22:03:43 +0100

    2. User.php mit ersten Methoden Ideen

---
+++ User.php
@@ -0,0 +1,68 @@
+<?php
+
+class User
+{
+    private $email;
+    private $password;
+
+    public function __construct($email, $password) {
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public static function get($email, $password)
+    {
+
+    }
+
+    public function login() {
+        session_start();
+
+    }
+
+    public static function logout() {
+        session_start();
+
+    }
+
+    public static function isLoggedIn() {
+        session_start();
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+
+
+
+}
